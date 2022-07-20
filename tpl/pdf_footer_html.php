6be072c0138d6362452c98d1c7f2abd5348a0fc0
gizmore <gizmore@wechall.net>
2022-07-20 07:55:35 +0200

    '7.0.0-r1374'

--- tpl/pdf_footer_html.php
+++ tpl/pdf_footer_html.php
@@ -13,16 +13,16 @@ $a = $ma->cfgAddress();
 <table>
   <tr>
     <td><?=sitename()?></td>
-    <td><?=$a->display('address_name')?></td>
+    <td><?=$a->gdoDisplay('address_name')?></td>
     <td><?=sitename()?></td>
   </tr>
   <tr>
-    <td><?=$a->display('address_street')?></td>
+    <td><?=$a->gdoDisplay('address_street')?></td>
     <td><?=t('pdfbill_payment')?></td>
     <td><?=t('vat')?>: <?=$mp->cfgVat()?></td>
   </tr>
   <tr>
-    <td><?=$a->display('address_zip').' '.$a->display('address_city')?></td>
+    <td><?=$a->gdoDisplay('address_zip').' '.$a->gdoDisplay('address_city')?></td>
     <td><?=$order->getPaymentModule()->displayPaymentMethodName()?></td>
     <td><?=t('financial_office') . $mp->cfgVatOffice()?></td>
   </tr>
