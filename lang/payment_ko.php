0961201dbb7cdd0e80ef9699faf0016c5985cf58
gizmore <gizmore@wechall.net>
2026-09-18 04:06:57 +0200

    'more korean'

---
+++ lang/payment_ko.php
@@ -0,0 +1,101 @@
+<?php
+return [
+	'module_payment' => '결제',
+	'order_title' => '주문 제목',
+	'paid_at' => '지불 시간',
+	'executed_at' => '실행 시간',
+	'price' => '가격',
+	'err_order' => '주문을 찾을 수 없습니다.',
+	'err_orderable' => '먼저 구매하실 상품을 선택해주세요.',
+	'card_title_order' => '귀하의 주문',
+	'payment' => '결제',
+	'payment_fee' => '수수료(%)',
+	'total' => '합계',
+	'msg_order_execute' => '귀하의 주문이 성공적으로 실행되었습니다.',
+	'mt_payment_order' => '주문',
+	'btn_execute' => '실행',
+	'link_your_orders' => '귀하의 주문',
+	'link_orders' => '주문',
+	'btn_order' => '주문',
+	'err_order_pending' => '귀하의 결제가 아직 보류 중입니다.',
+	'btn_pdf_bill' => '레흐눙',
+	'pdfbill_filename' => 'Rechnung_%s_%s_%s.pdf',
+	'pdf_subject_payment' => '자룽스보르강',
+	'pdf_subtitle_paid' => 'Abschlussrechnung',
+	'pdf_subtitle_unpaid' => 'Vorläufige Rechnung',
+	'billing_mail_sender_name' => '레치눙겐 %s',
+	'billing_mail_reciver_name' => 'Rechnungsstelle %s',
+	'mail_subj_payment_bill' => '[%s] 검색 - %s',
+	'mail_body_payment_bill' => '안녕하세요 %s 님,
+	
+Sie haben eine Bestellung auf %s aufgegeben.
+	
+---------------------------------------
+	
+아티켈: %s
+	
+현재 상태: %s
+	
+Zahlungs방법: %s
+	
+---------------------------------------
+	
+Im Anhang은 Du eine vorläufige Rechnung을 찾았습니다.
+Es ist in Ordnung diese Email zu ignorieren, Falls Sie doch nicht bestellen möchten.
+	
+Sollten Sie die Bestellung nicht aufgegeben haben, 그래서 wenden Sie sich bitte an den support.
+	
+비엘레 그뤼세
+%2$s 팀',
+	'mail_subj_payment_bill_paid' => '[%s] 검색 - %s',
+	'mail_body_payment_bill_paid' => '안녕하세요 %s 님,
+	
+이 앱은 %s에서 %s에 걸쳐 Rechnung되었습니다.
+	
+비엘렌 덩크!
+	
+Dein Auftrag wurde ausgeführt und im Anhang finden Sie die abschliessende Rechnung für Ihre Unterlagen.
+	
+비엘레 그뤼세
+%3$s 팀',
+	'mail_subj_payment_bill_paid_staff' => '검색 - %s - %s',
+	'mail_body_payment_bill_paid_staff' => '안녕하세요 %s 님,
+	
+Eine Rechnung wurde bezahlt.
+	
+너처: %s
+	
+아티켈: %s
+	
+가격: %s
+	
+Zahlungs방법: %s
+	
+Im Anhang die Rechnung für die Unterlagen.',
+	'article' => '아티켈',
+	'ordered_at' => '베스텔트 오전',
+	'financial_office' => '금융',
+	'pdfbill_company_underline' => '<u>%s — %s — %s %s</u>',
+	'pdfbill_ordered_at' => '기록 데이터: %s',
+	'pdfbill_executed_at' => '목록데이터: %s',
+	'pdfbill_customer' => '전체 숫자: %s',
+	'pdfbill_number' => '기록 번호: %s',
+	'pdfbill_number_hint' => 'Bitte bei Zahlungen und Schriftverkehr stets angeben.',
+	'pdfbill_pay_until' => 'Bitte begleichen Sie den Rechnungsbetrag bis zum %s.',
+	'pdfbill_payment' => '자룽스바이제',
+	'pdforder_sum_netto' => 'Sume netto',
+	'pdforder_sum_tax' => 'zzgl. %s%% MwSt.',
+	'pdforder_sum_brutto' => '썸 브루또',
+	'order_num' => 'Rechnungsnummer',
+	'order_num_short' => 'R.Nr.',
+	'order_xtoken' => '레크눙스베르바이스',
+	'order_address' => '레크눙사드레스',
+	'order_price_paid' => '베레이트 게잘트',
+	'order_needs_address_first' => 'Bitte erstellen Sie zuerst eine Rechnungsadresse',
+	'div_order_section' => '베스텔다텐',
+	'msg_order_edited' => 'Die Bestellung wurde bearbeitet.',
+	'purchase' => '구매',
+	'err_order_cancel' => 'Sie können diese Bestellung nicht mehr stornieren.',
+	'list_payment_yourorders' => '귀하의 %s 주문',
+	'view_order' => '주문 보기',
+];
